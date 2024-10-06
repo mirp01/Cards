66c451d55add66d8ae65cd03fa64ba7d26b1deac
Jacob <a01643891@tec.mx>
2024-10-06 05:12:49 -0600

    Initial card Object initialization

--- Assets/Scripts/CardDataBase.cs
+++ Assets/Scripts/CardDataBase.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CardDatabase : MonoBehaviour
 {
-    public static List<Card> Cards = new List<Card>();
+    public static List<CardInfo> Cards = new List<CardInfo>();
     public Sprite[] images;
 
     public static CardDatabase Instance {
@@ -23,10 +23,14 @@ public class CardDatabase : MonoBehaviour
         for(int family = 1; family < 4; family++){
             for(int border = 1; border < 4; border++){
                 for(int number = 1; number < 6; number++){
-                    Cards.Add(new Card(id, family, border, number, images[id]));
+                    Cards.Add(new CardInfo(id, family, border, number, images[id]));
                     id++;
                 }
             }
         }
     }
+
+    public Sprite getImage(int Id){
+        return Cards[Id].image;
+    } 
 }
--- Assets/Scripts/CardInfo.cs
+++ Assets/Scripts/CardInfo.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
-public class Card : MonoBehaviour
-{
+[System.Serializable]
+public class CardInfo{
     public int id; // 1 to 45
     public int family; // 1:hunter  , 2: wizard , 3: artificies
     public int number; // 1 to 5
     public int border;  // 1: border1, 2: border2, 3:border3
+
     public Sprite image;
-    public Image imageDisplay;
 
-    void Start() {
-        imageDisplay.sprite = CardDatabase.Instance.getImage(id);
+    public CardInfo(){
+
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public CardInfo(int Id, int Family, int Border, int Number, Sprite Image){
+        this.id = Id;
+        this.family = Family;
+        this.border = Border;
+        this.number = Number;
+        this.image = Image;
     }
+
 }

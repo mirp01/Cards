9e9c26b556d06ca88cd4c8fd13f09a49f151f1cd
Jacob <a01643891@tec.mx>
2024-10-06 10:14:19 -0600

    Deck Functionality

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
 
-    public List<int> activeDeck;
+    public List<int> shuffleDeck;
+    public Queue<int> activeDeck;
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < CardDatabase.Cards.Count; i++){
-            activeDeck.Add(i);
+            shuffleDeck.Add(i);
         }
         Shuffle();
+        FillDeck();
     }
 
     // Update is called once per frame
@@ -24,12 +26,28 @@ public class Deck : MonoBehaviour
     void Shuffle(){
         int temp_value;
         int random_index;
-        int size = activeDeck.Count;
+        int size = shuffleDeck.Count;
         for (int i = 0; i < size; i++){
-            temp_value = activeDeck[i];
+            temp_value = shuffleDeck[i];
             random_index = Random.Range(0, size);
-            activeDeck[i] = activeDeck[random_index];
-            activeDeck[random_index] = temp_value;
+            shuffleDeck[i] = shuffleDeck[random_index];
+            shuffleDeck[random_index] = temp_value;
         }
+    }
+
+    void FillDeck(){
+        int size = shuffleDeck.Count;
+        for (int i = 0; i < size; i++){
+            activeDeck.Enqueue(shuffleDeck[i]);
+        }
+    }
+
+    public int PullCard(){
+        int value = activeDeck.Dequeue();
+        return value;
+    }
+
+    public void ReturnCard(int cardId){
+        activeDeck.Enqueue(cardId);
     }
 }

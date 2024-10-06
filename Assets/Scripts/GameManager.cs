34113e5b79adb8afc24cc7c34e7ad06a25679a84
Jacob <a01643891@tec.mx>
2024-10-06 15:53:51 -0600

    Initial function to check for wins

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -124,5 +124,17 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void checkUserWon(){
+        int tempFam1;
+        int tempFam2;
+        int tempFam3;
+
+        for (int i = 0; i < 3 ; i++){
+            for(int j = 0; j < 3; j++){
+                //winsPlayer[i][j];
+            }
+        }
+    }
+
 }   
 
